ee0b9d6daa4dfb05ca25a386c00db94c5dde21c3
cnGoku <406342509@qq.com>
2015-04-06 14:14:53 +0800

    new port
    to deploy in Azure

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var http = require('http');
 var fs = require('fs');
+var port = process.env.PORT || 8000
 
 var server = http.createServer(function (req, res) {
     var stream = fs.createReadStream(__dirname + '/data.txt');
     stream.pipe(res);
 });
+server.listen(port);
